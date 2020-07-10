66f0f72a730734af056d96698b030c574dae67e4
Daniel Abrantes <daniel.abrantes95@gmail.com>
2020-07-10 09:34:36 +0100

    design changes

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,14 +1,18 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
-// Import needed templates
-import '../../ui/pages/exposed/not-found/not-found.js';
-
 // Set up all routes in the app
 FlowRouter.route('/', {
   name: 'Landing',
   action() {
     BlazeLayout.render('Exposed_layout', { main: 'Landing' });
+  },
+});
+
+FlowRouter.route('/login', {
+  name: 'Login',
+  action() {
+    BlazeLayout.render('Exposed_layout', { main: 'Login' });
   },
 });
 
--- imports/ui/pages/index.js
+++ imports/ui/pages/index.js
@@ -1,3 +1,4 @@
 import './exposed/landing/landing';
+import './exposed/login/login';
 import './exposed/not-found/not-found';
 
