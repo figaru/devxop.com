df7dc1f46a8a1a5b06df2b9bd9920b06f4439868
Daniel Abrantes <daniel.abrantes95@gmail.com>
2020-08-12 13:57:43 +0100

    modal js

--- imports/ui/utils/modal.js
+++ imports/ui/utils/modal.js
@@ -7,9 +7,9 @@ hideModal = function (id) {
 
 }
 
-showModal = function (id) {
+/* showModal = function (id) {
     var myModalEl = document.getElementById(id);
     var modal = bootstrap.Modal.getInstance(myModalEl);
     modal.show();
 
-}
+} */
